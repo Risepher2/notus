6020fd7eecf0fa3e5e20dc898dc12b7fd358b2e9
Archie <54244039+variakle@users.noreply.github.com>
2019-10-05 22:09:04 +0100

    new cmd

---
+++ commands/info/stats.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "stats",
+    category: "info",
+    description: "Shows bot's stats",
+    run: async (client, message, args) => {
+        message.channel.send(`This command is under development. Please try again later.`);
+
+    }
+}
