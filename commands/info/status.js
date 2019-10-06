6985ad11868fdc3f4a9c2413e4b45f4bb0e6f3af
Archie <54244039+variakle@users.noreply.github.com>
2019-10-06 14:21:05 +0100

    Delete status.js

--- commands/info/status.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "status",
-    category: "info",
-    description: "Shows bot's status",
-    run: async (client, message, args) => {
-        message.channel.send(`This command is under development. Please try again later.`);
-
-    }
-}
