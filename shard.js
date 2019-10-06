6babee178aee7e87ffca93e009bdbd98f92375db
Archie <54244039+variakle@users.noreply.github.com>
2019-10-06 14:21:15 +0100

    Delete shard.js

--- shard.js
+++
@@ -1,5 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./index.js', { token: 'NjI5MTAwNTc5NjMyODQwNzA0.XZU1nA.Z4NWaJIxRyiOizuZpsVwo1srMUE' });
-
-manager.spawn();
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}.`));
