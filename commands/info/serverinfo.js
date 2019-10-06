ffa3bd8eee249067dcd638c7a3eaae9a41ce6d9e
Archie <54244039+variakle@users.noreply.github.com>
2019-10-06 14:48:30 +0100

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -8,13 +8,13 @@ module.exports = {
         let sicon = message.guild.iconURL;
         let embed = new RichEmbed()
         .setTitle("Server Information")
-        .setDescription(`Information about` + message.guild.name)
+        .setDescription(`Information about ` + message.guild.name)
         .setColor("#e3ae42")
         .setThumbnail(sicon)
-        .addField("Server Name", message.guild.name)
-        .addField("Created On", message.guild.createdAt)
-        .addField("You Joined", message.member.joinedAt)
-        .addField("Total Members", message.guild.memberCount);
+        .addField("Server Name", message.guild.name, true)
+        .addField("Created On", message.guild.createdAt, true)
+        .addField("You Joined", message.member.joinedAt, true)
+        .addField("Total Members", message.guild.memberCount, true);
     
         message.channel.send(embed);
     }
