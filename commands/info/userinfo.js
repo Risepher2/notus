73f9b97c172bfba094ff3034fd66c7b08962a5c3
Archie <54244039+variakle@users.noreply.github.com>
2019-10-07 20:41:03 +0100

    Update and rename whois.js to userinfo.js

---
+++ commands/info/userinfo.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../../functions.js");
+
+module.exports = {
+    name: "userinfo",
+    aliases: ["whois"],
+    category: "info",
+    description: "Returns user information",
+    usage: "[username | id | mention]",
+    run: (client, message, args) => {
+        let uEmbed = new Discord.RichEmbed()
+        .setColor(colours.pink)
+        .setTitle("User Information")
+        .setThumbnail(message.author.displayAvatarURL)
+        .addField("Username", `${message.author.username}`, true)
+        .addField("Discriminator", `${message.author.discriminator}`, true)
+        .addField("User ID", `${message.author.id}`, true)
+        .addField("Status", `${message.author.presence.status}`, true)
+        .addField("Created At", `${message.author.createdAt}`, true)
+        .setFooter(`${bot.user.tag}`, bot.user.displayAvatarURL);
+        message.channel.send({embed: uEmbed});
+    }
+}
+
